e72377be84e681aa005d366db731cef0b40187d1
Dalton Senseman <drsenseman4328@eagle.fgcu.edu>
2019-05-16 18:27:12 -0400

    Main menu added so uses can select options.

--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -1,21 +1,52 @@
-// Main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿/**
+*	COP-2001 Procedural Project
+*	main.cpp
+*	Purpose: To create a inventory management system for OraclProduction Ltd. That allows for users to be create accounts and manage inventory. 
+*
+*	@author Dalton Senseman
+*	@version 0.0 5/16/2019
+*/
 
 #include "pch.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using namespace std;
+
+int main() {
+
+	bool mainMenuChoice = false;
+	int mainMenuUserSelcection;
+
+	do {
+
+		cout << " ______________________________________ " << endl;
+		cout << "|       Production Line Tracker        |" << endl;
+		cout << "|                                      |" << endl;
+		cout << "|    1. Add Employee Account           |" << endl;
+		cout << "|    2. Add Music Player               |" << endl;
+		cout << "|    3. Add Movie Player               |" << endl;
+		cout << "|    4. Display Production Statistics  |" << endl;
+		cout << "|    5. Exit                           |" << endl;
+		cout << "|______________________________________|" << endl;
+		cout << "     Enter your selection: " << flush;
+		cin >> mainMenuUserSelcection;
+		cout << " " << endl;
+
+		switch (mainMenuUserSelcection) { //main menu switch to select one of the 5 options above. 
+		case 1: cout << "Selection 1: Accounts" << endl;
+			break;
+		case 2: cout << "Selection 2: Music players" << endl;
+			break;
+		case 3: cout << "Selection 3: Movie players" << endl;
+			break;
+		case 4: cout << "Selection 4: Production stats" << endl;
+			break;
+		case 5: cout << "Selection Exit" << endl;
+			exit(0); //program is terminated
+			break;
+		default: cout << "Unexpected error has occured in menu selection, choose an option from 1-5 on the menu screen\nThen press the numerical choice on your keyboard and hit enter." << endl;
+		}
+	} while (mainMenuChoice == false);
+	
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
