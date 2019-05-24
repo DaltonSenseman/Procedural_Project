5fa60ce0a2e3c0daa2a885f92bbd93156dd1cd86
Dalton Senseman <43663619+DaltonSenseman@users.noreply.github.com>
2019-05-23 23:52:18 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Procedural_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Procedural_Project.dir/main.cpp.obj"
+  "Procedural_Project.pdb"
+  "Procedural_Project.exe"
+  "Procedural_Project.exe.manifest"
+  "Procedural_Project.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Procedural_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
